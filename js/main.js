30d9677d6e46358eedc7b71c4828d4a67feab366
rensramos <rensramos17@gmail.com>
2017-09-21 22:22:39 +0800

    Update js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,30 @@ $('#icon-transition').on('click', function () {
     $(this).toggleClass('open');
 });
 
-//On navbar scroll
-// $(function () {
-//     if (window.pageYOffset > 30 && $(document).width() >= 1024) {
-//         $('nav').addClass('navbar-shrink')
-//         $(window).scroll(function () {
-//             if ($(document).scrollTop() > 50 && $(document).width() >= 1024) {
-//                 $('nav').addClass('navbar-shrink');
-//             } else {
-//                 $('nav').removeClass('navbar-shrink');
-//             }
-//         });
-//     }
-//     else if (window.pageYOffset < 30 && $(document).width() >= 1024) {
-//         $(window).scroll(function () {
-//             if ($(document).scrollTop() > 50 && $(document).width() >= 1024) {
-//                 $('nav').addClass('navbar-shrink');
-//             } else {
-//                 $('nav').removeClass('navbar-shrink');
-//             }
-//         });
-//     }
-//     if ($(document).width() <= 991 && window.pageYOffset > 30 || $(document).width() <= 1024 && window.pageYOffset < 30) {
-//         $('nav').addClass('navbar-shrink');
-//     }
-// });
+$(window).on("load", function(){
+    $('.loader').fadeOut();
 
+    $('#bootstrap-touch-slider').bsTouchSlider();
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 1,
+        // Disable preloading of all images
+        preloadImages: false,
+        lazyLoading: true,
+        loop: true
+    });
+});
+
+
+$(document).ready(function () {
+    $('.menu-scroll').on('click', function(e){e.preventDefault();
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 75
+        }, 1500, 'easeInOutExpo');
+    });
+
+
+});
