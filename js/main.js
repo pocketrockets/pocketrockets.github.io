982f15a6ace36ea19950a6a71df3be41d45d2d3d
rensramos <rensramos17@gmail.com>
2017-09-25 17:18:56 +0800

    WIP

--- js/main.js
+++ js/main.js
@@ -11,6 +11,32 @@ $(document).ready(function () {
         }, 1500, 'easeInOutExpo');
 
     });
+
+    var swiperItemContainer = $(".swiper-link")
+    swiperItemContainer.on("click",function () {
+        var modalTarget = $(this).attr("data-target").substring(1);
+        // var modalProjectTitle = $(this).attr("data-project-title");
+        var modalLogoType = $(this).attr("data-logo-type");
+        var modalClientLogo = $(this).attr("data-client-logo");
+        var modalProjectDuration = $(this).attr("data-client-duration");
+        var modalProjectList = $(this).attr("data-project-list");
+        var modalGallery = $(this).attr("data-gallery");
+
+        //For modal
+        $(this).parents().parents().find(".modal.fade").attr("id",modalTarget);
+        $(this).parents().parents().find(".modal.fade").attr("aria-labelledby",modalTarget);
+        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-client-logo").attr({ src: modalClientLogo, class: modalLogoType + " modal-client-logo"});
+        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-list-container").html(modalProjectList);
+        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-gallery").attr("src",modalGallery);
+        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-project-duration").text(modalProjectDuration);
+
+    });
+
+
+    $(".swiper-link").on("mouseover",function () {
+        var imageHover = $(this).attr("data-hover");
+        $(this).find(".caption .caption-image").attr("src",imageHover);
+    });
 });
 
 $(window).on("load", function(){
@@ -43,42 +69,12 @@ $(window).on("load", function(){
 
     });
 
-    // $(".swiper-image").on("mouseover", function () {
-    //     $(this).attr('src', $(this).data("hover"));
-    // });
-    // $(".swiper-image").on("mouseout", function () {
-    //     $(this).attr('src', $(this).data("original"));
-    // });
-
     var bLazy = new Blazy({
         selector: '.b-lazy',
         success: function(ele){
             $(".b-lazy.b-loaded").parent().addClass("add-after");
         }
     });
-
-    $(".swiper-link").on("click",function () {
-        // modal id
-        var swiperContainer  = $("#swiper-container2");
-        var modalTarget = $(this).attr("data-target").substring(1);
-        // var modalProjectTitle = $(this).attr("data-project-title");
-        var modalLogoType = $(this).attr("data-logo-type");
-        var modalClientLogo = $(this).attr("data-client-logo");
-        var modalProjectDuration = $(this).attr("data-client-duration");
-        var modalProjectList = $(this).attr("data-project-list");
-        var modalGallery = $(this).attr("data-gallery");
-
-        $(this).parents().parents().find(".modal.fade").attr("id",modalTarget);
-        $(this).parents().parents().find(".modal.fade").attr("aria-labelledby",modalTarget);
-        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-client-logo").attr({ src: modalClientLogo, class: modalLogoType + " modal-client-logo"});
-        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-list-container").html(modalProjectList);
-        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-gallery").attr("src",modalGallery);
-        $(this).parents().parents().find(".modal.fade .modal-content .modal-body .modal-project-duration").text(modalProjectDuration);
-
-
-
-    });
-
 
 });
 
