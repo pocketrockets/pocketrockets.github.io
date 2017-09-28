abffd49e20b3cbda9415842fc1dd165e9d4f82ef
rensramos <rensramos17@gmail.com>
2017-09-28 18:50:06 +0800

    Fix navbar

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
-//Navbar burger transition
-$('#icon-transition').on('click', function () {
-    $(this).toggleClass('open');
-});
-
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
-
-    $('.navbar-link[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('.navbar-link').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-
-
-
     var swiperItemContainer = $(".swiper-link")
     swiperItemContainer.on("click",function () {
         var modalTarget = $(this).attr("data-target").substring(1);
@@ -124,19 +95,36 @@ $(window).on("load", function(){
 
 });
 
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('.navbar-right .navbar-link').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.navbar-right .navbar-link').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
+//navbar
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 100)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
         }
     });
-}
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 100
+    });
+
+
+})(jQuery); // End of use strict
 
 
